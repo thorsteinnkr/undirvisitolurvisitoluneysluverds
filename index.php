d1290118e11dd452db632f527bf10d69e3001244
Þorsteinn K. Kristinsson <36857516+thorsteinnkr@users.noreply.github.com>
2024-08-21 15:27:12 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
 <?php
-  $man=(isset($_REQUEST['man'])?$_REQUEST['man']:"2024M01");
+  header('Content-Type: text/html; charset=iso-8859-1');
+  $man=(isset($_REQUEST['man'])?$_REQUEST['man']:"2024M07");
   $far=(substr($man,0,4)-1).substr($man,4,3);
 
-  print("<style>a{text-decoration:none;}</style>");
-  print("<h2>Undirliðir vísitölu neysluverðs</h2>");
-  print("<h3>Breyting á 12 mánaða tímabili</h3>");
+  print("<html><head>");
+  print("<title>Undirli&eth;ir v&iacute;sit&ouml;lu neysluver&eth;s</title>");
+  print("<style>*{font-family:lato;color:#133942;} a{text-decoration:none;}</style>");
+  print("<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Lato:300,400,700,900' type='text/css'>");
+  print("</head><body>");
+  print("<h2>Undirli&eth;ir v&iacute;sit&ouml;lu neysluver&eth;s</h2>");
+  print("<h3>Breyting &aacute; 12 m&aacute;na&eth;a t&iacute;mabili</h3>");
   print("<h2>".$man." / ".$far."</h2>");
 
   $mydb=new mysqli("localhost","dbuser","dbpassword","dbname");
@@ -13,8 +18,7 @@
     exit();
   }
 
-  print("<table><tr>");
-  print("<td style='vertical-align:top;width:120px;'>");
+//  print("<td style='vertical-align:top;width:120px;'>");
 
   $sql="select distinct man from vnv order by man limit 1000 offset 12";
   $res=$mydb->query($sql);
@@ -23,10 +27,13 @@
     $heiti[]=$row['man'];
   }
   rsort($heiti);
+  print("<form name='val1' method='get' action='?'><select name='man' id='man'>");
   foreach($heiti as $v){
-    print("<a style='color:purple;text-decoration:underline;' href='?man=".$v."'>".$v."</a><br>");
+    print("<option value='".$v."' ".($man==$v?"selected":"").">".$v."</option>");
   }
+  print("</select><input class='land' type='submit' value='S&aelig;kja'></form>");
 
+  print("<table><tr>");
   print("<td style='vertical-align:top;'>");
 
   $listi=array();
@@ -41,21 +48,39 @@
   $res=$mydb->query($sql);
   while($row=$res->fetch_assoc()){
     if(isset($listi[$row['heiti']])){
-      $uttak[$row['heiti']]=$row['gildi']/$listi[$row['heiti']]-1;
+      if($listi[$row['heiti']]!=0){
+          $uttak[$row['heiti']]=$row['gildi']/$listi[$row['heiti']]-1;
+      }  else {
+          $uttak[$row['heiti']]=0;
+      }
     } else {
       $uttak[$row['heiti']]="0";
     }
   }
 
   $h="";
+  $p1="";
+  $p2="";
   foreach($uttak as $k=>$v){
     $gildi=number_format($v*100,1);
-    $style='color:green;';
-    if($gildi>2.5){
+    $style='color:#133942;';
+    if($gildi>7.5){
+      $style='color:red;background-color:yellow;';
+    }
+    else if($gildi>2.5){
       $style='color:red;';
     }
-    if($k=="Vísitala neysluverðs"){
-      $style.="font-weight:bold;font-size:16pt;";
+    if($gildi<0){
+      $style='color:green;';
+    }
+    if(substr($k,2,1)==" "){
+      $k2="<a style='".$style."' href='lidur.php?lidur=".$k."' target='_blank'>".$k."</a>";  
+      $p2.="<span style='".$style."font-size:16pt;'>".$k2.": ".$gildi."%</span> ".($gildi>2.5?str_repeat("&#128681",floor(($gildi)/2.5)):"")."<br>";
+    }
+    if(substr($k,0,1)=="V"){
+      $style.="font-weight:bold;font-size:16pt;;";
+      $k2="<a style='".$style."' href='lidur.php?lidur=".$k."' target='_blank'>".$k."</a>";
+      $p2.="<span style='".$style."font-size:16pt;'>".$k2.": ".$gildi."%</span> ".($gildi>2.5?str_repeat("&#128681",floor(($gildi)/2.5)):"")."<br>";
     }
     if(strpos($k," ")==2){
       $style.="font-weight:bold;font-size:16pt;";
@@ -69,10 +94,14 @@
     if(strpos($k," ")==5){
       $style.="padding-left:12px;font-size:11pt;";
     }
-    if($h!=substr($k,0,2)){ print("<hr>");$h=substr($k,0,2);}
-    $k="<a style='".$style."' href='lidur.php?lidur=".$k."' target='_blank'>".$k."</a>";
-      print("<span style='".$style."'>".$k.": ".$gildi."%</span> ".str_repeat("&#128681",floor(($gildi-0.01)/2.5))."<br>");
+    if($h!=substr($k,0,2)){ $p1.="<hr>";$h=substr($k,0,2);}
+      $k="<a style='".$style."' href='lidur.php?lidur=".$k."' target='_blank'>".$k."</a>";
+      $p1.="<span style='".$style."'>".$k.": ".$gildi."%</span> ".($gildi>2.5?str_repeat("&#128681",floor(($gildi)/2.5)):"")."<br>";
+
   }
+  print($p2);
   print("<td style='vertical-align:top;'>");
+  print($p1);
   print("</tr></table>");
+  print("</body></html>");
 ?>
