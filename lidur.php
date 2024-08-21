01c6fc4ce2de5a60dd693499c9dbda4d74f57f27
Þorsteinn K. Kristinsson <36857516+thorsteinnkr@users.noreply.github.com>
2024-08-21 15:27:52 +0000

    Update lidur.php

--- lidur.php
+++ lidur.php
@@ -1,10 +1,16 @@
 <?php
-
+  header('Content-Type: text/html; charset=iso-8859-1');
   $lidur=(isset($_REQUEST['lidur'])?$_REQUEST['lidur']:"Vísitala neysluverðs");
   //$far=(substr($man,0,4)-1).substr($man,4,3);
 
+  print("<html><head>");
+  print("<title>Undirli&eth;ir v&iacute;sit&ouml;lu neysluver&eth;s</title>");
+    print("<style>*{font-family:lato;color:#133942;} a{text-decoration:underline;color:purple;}</style>");
+  print("<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Lato:300,400,700,900' type='text/css'>");
+  print("</head><body>");
+  print("<h1><a href='/'>Undirli&eth;ir v&iacute;sit&ouml;lu neysluver&eth;s</a></h1>");
   print("<h2>".$lidur."</h2>");
-  print("<h3>Breyting á 12 mánaða tímabili</h3>");
+  print("<h3>Breyting &aacute; 12 m&aacute;na&eth;a t&iacute;mabili</h3>");
 
   $mydb=new mysqli("localhost","dbuser","dbpassword","dbname");
   if ($mydb -> connect_errno) {
@@ -21,17 +27,26 @@
   while($row=$res->fetch_assoc()){
     $gildi[$row['man']]=$row['gildi'];
   }
+  $n=0;
   foreach($gildi as $k=>$v){
     $far=(substr($k,0,4)-1).substr($k,4,3);
     if(isset($gildi[$far])){
       $p=number_format(($v/$gildi[$far]-1)*100,1);
-      $style="color:green";
-      if($p>2.5){
-        $style="color:red";
+      $style="color:#133942;";
+      if($p>7.5){
+        $style="color:red;background-color:yellow;";
       }
-      print("<span style='".$style."'>".$k.": ".$p."%</span>".str_repeat("&#128681",floor(($p-0.01)/2.5))."<br>");
+      else if($p>2.5){
+        $style="color:red;";
+      }
+      if($p<0){
+        $style="color:green;";
+      }
+      $n++;      
+      print("<span style='".$style."'>".$k." [".$v."]: ".$p."%</span>".($p>2.5?str_repeat("&#128681",floor(($p)/2.5)):"").($n%12==0?"<hr>":"<br>"));
     }
   }
 
   print("</tr></table>");
+  print("</body></html>");
 ?>
